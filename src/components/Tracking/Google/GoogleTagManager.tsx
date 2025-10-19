afba486e149f3dae4044b3fa71a973b26824dc91
github username <lhon001@gmail.com>
2025-10-19 16:31:28 -0400

    update gtm id

--- src/components/Tracking/Google/GoogleTagManager.tsx
+++ src/components/Tracking/Google/GoogleTagManager.tsx
@@ -1,10 +1,10 @@
 import Script from 'next/script';
 
-export default function GoogleTagManager({ GTM_ID }: { GTM_ID: string }) {
-  if (!GTM_ID) {
-    console.warn('GoogleTagManager ID is missing!');
-    return null;
-  }
+export default function GoogleTagManager() {
+  // if (!GTM_ID) {
+  //   console.warn('GoogleTagManager ID is missing!');
+  //   return null;
+  // }
 
   return (
     <>
@@ -17,7 +17,7 @@ export default function GoogleTagManager({ GTM_ID }: { GTM_ID: string }) {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${GTM_ID}');
+            })(window,document,'script','dataLayer','GTM-K2G3J9SS');
           `,
         }}
       />
